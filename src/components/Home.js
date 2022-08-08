3d079f0cb97abf114a5be27476ca97fbbb035552
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-08 17:45:43 +0200

    add styling for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,22 @@
 import React from "react";
 import Header from "./Header";
-import '../styles/Home.css';
+import "../styles/Home.css";
 
 const Home = () => (
   <div className="home-container">
-    <div className="color-overlay">
-      <Header />
-      <main>
-        <h1>Welcome to our page!</h1>
-        <p className="home-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
-        </p>
-      </main>
-    </div>
+    <Header />
+    <main>
+      <h1>Welcome to our page!</h1>
+      <p className="home-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </main>
   </div>
 );
 
