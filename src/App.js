24631203c482b98d759678959a912af70067558f
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-09 12:10:07 +0200

    setup reactrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import CalculatorPage from './components/CalculatorPage';
 import Quote from './components/Quote';
@@ -7,11 +8,15 @@ import './styles/App.css';
 class App extends React.PureComponent {
   render() {
     return (
-      <div className="App">
-        <Home />
-        <CalculatorPage />
-        <Quote />
-      </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="Home" element={<Home />} />
+            <Route path="Calculator" element={<CalculatorPage />} />
+            <Route path="Quotes" element={<Quote />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
-import '../styles/Header.css';
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
 
 const Header = () => (
   <header>
     <h1 className="header-title">Math Magicians</h1>
     <ul>
-      <li>Home</li>
-      <li>Calculator</li>
-      <li>Quote</li>
+      <li>
+        <Link to="/Home">Home</Link>
+      </li>
+      <li>
+        <Link to="/Calculator">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/Quotes">Quotes</Link>
+      </li>
     </ul>
   </header>
 );
