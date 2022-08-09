cc41c0b8b4c2f07de48c0056d10c723de0af44e1
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-09 15:42:43 +0200

    update home text

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,13 @@ const Home = () => (
     <main>
       <h1>Welcome to our page!</h1>
       <p className="home-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Remember that one time in math class when the teacher asked you
+        what&apos;s 2 + 2, and you said 5? Then everyone in the class laughed at
+        you, and you went home crying. Well no more of that! Introducing math
+        magicians, it&apos;s a single page app that fullfils all your second
+        grade math needs. And if you ever lose motivation to do more math, check
+        out the quotes section that provides a fresh new math quote for
+        inspiration.
       </p>
     </main>
   </div>
